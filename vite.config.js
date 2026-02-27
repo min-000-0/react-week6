bacca50774699a21eb458667312443616820b553
rei <rei@reideMacBook-Air.local>
2026-02-28 00:12:55 +0800

    chore: set base for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-week5/",
+  base: "/react-week6/",
   plugins: [react()],
 });
