0de8ef2a7f53c1f9f7fdba51322505cb4defbc68
rei <rei@reideMacBook-Air.local>
2026-02-27 11:52:58 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-week3/",
+  base: "/react-week4/",
   plugins: [react()],
 });
